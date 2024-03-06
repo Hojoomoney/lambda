ef84fed104ae39c0be4d25bacb12b81979fa37f6
Hojoomoney <kimhojoo51@gmail.com>
2024-03-06 20:20:30 +0900

    music chart crawling

--- crawler/CrawlerController.java
+++ crawler/CrawlerController.java
@@ -1,10 +1,24 @@
 package crawler;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class CrawlerController {
-    public Map<String,?> findBugsMusic(Scanner sc) {
-        return null;
+    private CrawlerServiceImpl crawlerService = CrawlerServiceImpl.getInstance();
+
+    public Map<String,?> findBugsMusic(Scanner sc) throws IOException {
+        System.out.println("크롤링할 주소를 입력하시오.");
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("URL",sc.next());
+        return crawlerService.findNamesFromWeb(paramMap);
+    }
+
+    public Map<String,?> findMelonMusic(Scanner sc) throws IOException {
+        System.out.println("크롤링할 주소를 입력하시오.");
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("URL",sc.next());
+        return crawlerService.findNamesFromWebMelon(paramMap);
     }
 }
--- crawler/CrawlerView.java
+++ crawler/CrawlerView.java
@@ -1,17 +1,20 @@
 package crawler;
 
-import member.MemberController;
 
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
 public class CrawlerView {
-    public static void main(Scanner sc) {
+    public static void main(Scanner sc) throws IOException {
         CrawlerController ctrl = new CrawlerController();
         while(true){
             System.out.println("[메뉴] 0-종료\n" +
                     " 1-벅스뮤직\n" +
-                    " 2-로그인\n" +
+                    " 2-멜론\n" +
                     " 3-ID검색\n" +
                     " 4-비번변경\n" +
                     " 5-탈퇴\n" +
@@ -26,10 +29,26 @@ public class CrawlerView {
                 case "1" :
                     System.out.println("1-벅스뮤직");
                     Map<String, ?> map = ctrl.findBugsMusic(sc);
+                    Iterator<Element> rank = (Iterator<Element>) map.get("rank");
+                    Iterator<Element> artist = (Iterator<Element>) map.get("artist");
+                    Iterator<Element> title = (Iterator<Element>) map.get("title");
+
                     System.out.println("벅스뮤직 결과 : ");
+                    while(rank.hasNext()){
+                        System.out.println(rank.next().text() + "위 " + artist.next().text() + " - " + title.next().text());
+                    }
                     break;
                 case "2" :
-                    System.out.println("2");
+                    System.out.println("2-멜론");
+                    map = ctrl.findMelonMusic(sc);
+                    rank = (Iterator<Element>) map.get("rank");
+                    artist = (Iterator<Element>) map.get("artist");
+                    title = (Iterator<Element>) map.get("title");
+
+                    System.out.println("멜론차트 결과 : ");
+                    while(rank.hasNext()){
+                        System.out.println(rank.next().text() + "위 " + artist.next().text() + " - " + title.next().text());
+                    }
                     break;
                 case "3" :
                     System.out.println("3");
